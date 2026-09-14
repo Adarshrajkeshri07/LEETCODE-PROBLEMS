4c9c52d8d74676c9ddd564dff443f61837a5b121
Adarshrajkeshri07 <rajadarshkeshri@gmail.com>
2026-09-14 19:14:01 +0530

    using hashmap two sum

--- easy/leetcode1.cpp
+++ easy/leetcode1.cpp
@@ -1,3 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std ; 
+int main(){
+    vector<int> a = {2,7,9,11,5} ; 
+    int n = a.size(); 
+    bool match = false ; 
+    unordered_map<int , int> m ; 
+     pair<int ,int> ans ; 
+    int target = 16 ; 
+    for(int i = 0 ; i < n ; i++){
+        int fisrt  = a[i] ; 
+        int sec = target - fisrt ; 
+        if(m.find(sec) != m.end()){
+            ans = {i , m[sec]} ;
+            match = true;
+            break;
+           
+        }
+        m[fisrt] = i ; 
+    }
+   if(match){
+    cout << ans.first << " " << ans.second ; 
+   }else{
+    cout << " invalid" ; 
+   }
+    return 0 ; 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //bruteforce approach //////
 // #include<iostream>
 // #include<vector>
@@ -49,32 +94,32 @@
 // }
 // ;
 
-#include<iostream>
-using namespace std ;
-void twosum(int a[] , int size , int target) {
-    int st = 0 , end = size -1 ; 
-    int sum = 0 ; 
-    while(st < end){
-        sum = a[st] + a[end] ; 
-        if(sum < target){
-            st++;
-        }else 
-        if(sum > target){
-            end--;
-        }
-        else {
-            cout <<  " matched";
-            return ;
+// #include<iostream>
+// using namespace std ;
+// void twosum(int a[] , int size , int target) {
+//     int st = 0 , end = size -1 ; 
+//     int sum = 0 ; 
+//     while(st < end){
+//         sum = a[st] + a[end] ; 
+//         if(sum < target){
+//             st++;
+//         }else 
+//         if(sum > target){
+//             end--;
+//         }
+//         else {
+//             cout <<  " matched";
+//             return ;
             
            
-        }
-    }
-    cout <<  "not " ; 
-}
- int main(){
-    int nums[3] = { 3 , 5 , 9 } ;
-    twosum(nums,3,12);
+//         }
+//     }
+//     cout <<  "not " ; 
+// }
+//  int main(){
+//     int nums[3] = { 3 , 5 , 9 } ;
+//     twosum(nums,3,12);
     
       
-     return 0 ;
- }
+//      return 0 ;
+//  }
