ed5b13d36905537b0348182d34d2b4872be9830c
Adarshrajkeshri07 <rajadarshkeshri@gmail.com>
2026-09-22 21:41:14 +0530

    leetcode560

--- medium/leetcode560.cpp
+++ medium/leetcode560.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include<unordered_map>
 using namespace std;
-
-class Solution
-{
-public:
-    int subarraySum(vector<int> &a, int k)
-    {
+class Solution{
+    public :
+    int subarraySum(vector<int> &a , int k){
         int n = a.size() ; 
-        int vote = 0  ; 
+        vector<int> ps(n ,0) ; 
+        int vote = 0 ; 
+        ps[0] = a[0] ; 
+        for(int i = 1 ; i < n ; i++){
+            ps[i] = ps[i - 1] + a[i] ; 
+        }
+        unordered_map<int , int>m ;
+        m[0] = 1;
         for(int i = 0 ; i < n ; i++){
-            int sum = 0;
-            for(int j = i ; j < n ; j++){
-                sum += a[j] ;
-                if (sum == k)
-                {
-                    vote++;
-                }
-            }   
+            int val = ps[i] - k ; 
+            if(m.find(val) != m.end()){
+                vote += m[val] ; 
+            }
+            m[ps[i]]++ ; 
         }
         return vote ; 
     }
+
 };
 int main(){
     vector<int> a = {2,4,17,8,6} ; 
@@ -30,3 +32,35 @@ int main(){
     int ans = obj.subarraySum(a , k) ; 
     cout << ans ; 
 }
+
+
+
+
+
+
+
+
+
+ //brute force //////////////
+// class Solution
+// {
+// public:
+//     int subarraySum(vector<int> &a, int k)
+//     {
+//         int n = a.size();
+//         int vote = 0;
+//         for (int i = 0; i < n; i++)
+//         {
+//             int sum = 0;
+//             for (int j = i; j < n; j++)
+//             {
+//                 sum += a[j];
+//                 if (sum == k)
+//                 {
+//                     vote++;
+//                 }
+//             }
+//         }
+//         return vote;
+//     }
+// };
